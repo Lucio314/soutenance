2542fe21ce776018240a2ea997e88016e772c62f
Lucio314 <morinoassilamehou@gmail.com>
2024-04-29 23:46:15 +0100

    Connexion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'technician']);
+            $table->enum('role', ['admin', 'technician', 'company'])->default('company');
             $table->boolean('is_active')->default(false);
             $table->rememberToken();
             $table->timestamps();
